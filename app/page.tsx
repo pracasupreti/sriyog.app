5ebff17c89e2e85784a9e11d9ccabf0bba9b3d15
Gaurav Thapa <gauravthapa926@gmail.com>
2025-05-21 14:01:30 +0545

    Fix: Adjust mobile image responsiveness and layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
    <>
     <div
-        className="desktop-section mx-4 md:mx-35 my-8 md:my-10 flex flex-col md:flex-row justify-center md:justify-between items-center">
+        className="desktop-section mx-4 md:mx-35 my-8 md:my-10 flex flex-col-reverse md:flex-row justify-center md:justify-between items-center">
         <div className="flex flex-col items-center md:items-center space-y-6">
-            <h1 className=" text-md md:text-4xl  text-center md:text-left">Find Skilled Professionals</h1>
+            <h1 className=" text-md md:text-4xl  text-center md:text-left mt-4">Find Skilled Professionals</h1>
             <p className="text-xl text-center md:text-left">100K+ Downloads</p>
             <p className="font-semibold text-center md:text-left">Now India Added</p>
             <div className="flex space-x-4">
@@ -13,11 +13,11 @@ export default function Home() {
                 </a>
                 <a target="_blank" href="https://bit.ly/sriyogios">
                  <img src="/images/apple.png" className="h-16" alt=""/>
-                 </a>
+                 </a>   
             </div>
         </div>
         <div className="mobile-image">
-            <img src="/images/mobile.png" className="h-64 md:h-96" alt=""/>
+            <img src="/images/mobile.png" className="h-100 md:h-96" alt=""/>
         </div>
     </div>
    </>

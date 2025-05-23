ed77a394e8fc8abb6d408133f0861b7d41188776
Gaurav Thapa <gauravthapa926@gmail.com>
2025-05-23 11:56:39 +0545

    updated pitch page section

--- app/pitch/page.tsx
+++ app/pitch/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+        <section className="w-full border-b bg-foreground">
+            <section className="container mx-auto py-5 pb-0 text-center">
+                <h1 className="text-xl sm:text-2xl md:text-2xl lg:text-2xl font-semibold text-backgorund">Pitch Deck</h1>
+            </section>
+        </section>
+        <section className="container mx-aut mt-0 pt-0">
+            <div className="relative pb-[40%] h-0 overflow-hidden max-w-full">
+                <iframe
+                src="https://docs.google.com/presentation/d/e/2PACX-1vSqzjiZR8-kGSStsO26UPbueZ3vZdYrTZ_maQxw5TeQaazVkZQHot9vgOFgbPX_KOtBQfb6JSdFRdLq/embed?start=false&loop=false&delayms=3000"
+                allowFullScreen
+                className="absolute top-0 left-0 w-full h-full"
+                ></iframe>
+            </div>
+        </section>
+
+
+            
+    </div>
   )
 }
 

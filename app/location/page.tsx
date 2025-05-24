c672532b52f4f3aa800cffcde76089a3fa992877
Gaurav Thapa <gauravthapa926@gmail.com>
2025-05-24 20:49:49 +0545

    location page updated

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,8 +1,11 @@
+import LocationMap from '@/components/LocationMap'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <LocationMap/>
+    </div>
   )
 }
 

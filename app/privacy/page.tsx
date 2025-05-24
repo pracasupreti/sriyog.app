566d03aad0ddaa862d74d9ecdbba49d9cfc41c49
Gaurav Thapa <gauravthapa926@gmail.com>
2025-05-24 20:59:23 +0545

    privacy policy section updated

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,8 +1,11 @@
+import PrivacyPolicy from '@/components/Policy'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <PrivacyPolicy/>
+    </div>
   )
 }
 
